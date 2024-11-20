dc0ac277e1f1a6c06d4b06c3e8acc5414decbe8c
TristanKatte <tristankatte@hotmail.com>
2024-11-20 16:02:14 +0100

    install

---
+++ chat-app/src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+    import { onMount } from 'svelte';
+    import Talk from 'talkjs';
+
+    onMount(() => {
+    Talk.ready.then(() => {
+        const session = new Talk.Session({
+        appID: 'YOUR_APP_ID',
+        me: currentUser,
+    });
+  });
+});
+
+  let chatboxEl;
+  </script>
+
+<main>
+    <div bind:this={chatboxEl}></div>
+  </main>
