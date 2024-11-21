d6102ad6588bf1f368ec5d8f260fe1407102409b
TristanKatte <tristankatte@hotmail.com>
2024-11-21 10:59:29 +0100

    .

--- chat-app/src/routes/+page.svelte
+++ chat-app/src/routes/+page.svelte
@@ -11,6 +11,35 @@
   });
 });
 
+const currentUser = new Talk.User({
+  id: '1920',
+  name: 'Oliver Johnson',
+  email: 'oliverjohnson@example.com',
+  photoUrl: 'oliver.jpeg',
+  welcomeMessage: 'Hey there!',
+  role: 'default',
+});
+
+const otherUser = new Talk.User({
+  id: '2017',
+  name: 'Abby Klein',
+  email: 'abbyklein@example.com',
+  photoUrl: 'abby.jpeg',
+  welcomeMessage: 'Hello, how are you today?',
+  role: 'default',
+});
+
+const conversation = session.getOrCreateConversation(
+  Talk.oneOnOneId(currentUser, otherUser)
+);
+
+conversation.setParticipant(currentUser);
+conversation.setParticipant(otherUser);
+
+const chatbox = session.createChatbox();
+chatbox.select(conversation);
+chatbox.mount(chatboxEl);
+
   let chatboxEl;
   </script>
 
